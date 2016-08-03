ba5a2d0957d6789529abe103de383636864fd499
Gabriel Schrock <gabrielschrock@gmail.com>
2016-08-02 19:36:50 -0700

    (style) Add buttons to navbar component (#20)
    * (feat) Add navbar component
    
    * (feat) Add buttons to navbar component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,14 +4,15 @@
 // var NavItem = require('react-materialize').NavItem;
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Navbar, NavItem } from 'react-materialize';
+import { Navbar, NavItem, Button } from 'react-materialize';
 
 var Topbar = React.createClass({
   render: function() {
     return (
-      <Navbar brand='logo' right>
-        <NavItem href='get-started.html'>Getting started</NavItem>
-        <NavItem href='components.html'>Components</NavItem>
+      <Navbar className='pink darken-1 parent' brand={'Cheri\'s Creative Celebrations'} right>
+        <Button className='navButton' waves='light'>About</Button>
+        <Button className='navButton' waves='light'>Contact</Button>
+        <Button className='navButton' waves='light'>Testimonials</Button>
       </Navbar>
     );
   }
