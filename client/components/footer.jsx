a97b2c602acdf7147033fa50e4b9b18503212a9d
amenotu <kyabut@gmail.com>
2016-08-05 11:36:08 -0700

    (feat) Improve footer component

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -21,11 +21,11 @@ var Bottombar = React.createClass({
         <p className="contact-footer">Available by appointment</p>
         <p className="contact-footer">Cheri Winter</p>
         <p className="contact-footer">Phone: (707)###-####</p>
-        <p className="contact-footer">Email: cheri.creativecelebrations@gmail.com</p>
+        <p className="contact-footer">Email: cheri.creativecelebrations@example.com</p>
         <p className="photographers">
         Photos provided by <a href="https://www.facebook.com/AudraVonGlahnPhotography" target="_blank">Audra Von Glahn Photography</a>, <a href="https://www.facebook.com/MindyBreslinPhotography" target="_blank">Mindy Breslin Photography</a>, <a href="http://www.foothillphotographyonline.com/" target="_blank">Foothill Photography</a>, and <a href="http://www.rachellomeli.com/" target="_blank">Rachel Lomeli Photography</a>
         </p>
-        <p className="webdevs">Website Design provided by <a href="#!" target="_blank">Kristine Yabut</a> and <a href="#!" target="_blank">Gabriel Schrock</a>
+        <p className="webdevs">Website Design provided by <a href="https://www.linkedin.com/in/kyabut1" target="_blank">Kristine Yabut</a> and <a href="https://www.linkedin.com/in/gabrielschrock" target="_blank">Gabriel Schrock</a>
         </p>
       </Footer>
     );
