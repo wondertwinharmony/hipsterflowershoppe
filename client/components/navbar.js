6fe8290c8083942a0301c0cd28f75b33304886eb
Gabriel Schrock <gabrielschrock@gmail.com>
2016-08-22 10:57:10 -0700

    (style) Update navbar title and button design (#28)

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -11,11 +11,26 @@ export default class Topbar extends Component {
   render() {
     console.log("state in topbar: ", this.state);
     return (
+      <div>
+        <div>
+          <svg id="pink">
+              <text x="0" y="0">
+                <tspan x="1.8em" y="1em">CHERI'S</tspan>
+                <tspan x="1.2em" y="2em">CREATIVE</tspan>
+                <tspan x="0.5em" y="3em">CELEBRATIONS</tspan>
+              </text>
+              <pattern id="pink-pattern" patternUnits="userSpaceOnUse"
+                         width="500" height="281" viewbox="0 0 500 281">
+              <image href="http://casablancasunset.com/wp-content/uploads/2013/12/washed-out-paracosm.jpg" width="500" height="281" />
+            </pattern>
+          </svg>
+        </div>
       <Navbar className='pink darken-1 parent' brand={'Cheri\'s Creative Celebrations'} right>
         <Button className='navButton' waves='light'>About</Button>
         <Button className='navButton' waves='light'>Contact</Button>
         <Button className='navButton' waves='light'>Testimonials</Button>
       </Navbar>
+      </div>
     );
   }
 }
