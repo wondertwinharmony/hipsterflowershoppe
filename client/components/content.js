069dbca72503618fd237a6738faa50cd0eea3e20
gschrock <gabrielschrock@gmail.com>
2016-08-22 10:26:28 -0700

    (style) Update navbar title and button design

--- client/components/content.js
+++ client/components/content.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Footer, Navbar, Button } from 'react-materialize';
+import { Footer, Button, Row, Col} from 'react-materialize';
 import FontAwesome from 'react-fontawesome';
 import Home from './home.js';
 import About from './about.js';
@@ -26,16 +26,35 @@ class Content extends Component {
     } else if (this.state.currentPage === '#!/about') {
       partial = <About />;
     }
-
+    
+    // Need to decide which of these to use in final title implementation
+    // http://casablancasunset.com/wp-content/uploads/2013/12/washed-out-paracosm.jpg
+    // https://s3-us-west-2.amazonaws.com/s.cdpn.io/131611/flower-bloom.gif
+    
     return (
       <div>
-      <Navbar className='pink darken-1 parent' brand={'Cheri\'s Creative Celebrations'} right>
-        <Button className='navButton' waves='light' href="#!/" onClick={this.onContentChange}>Home</Button>
-        <Button className='navButton' waves='light' href="#!/about" onClick={this.onContentChange}>About</Button>
-        <Button className='navButton' waves='light'>Balloon Decor</Button>
-        <Button className='navButton' waves='light'>Floral Designs</Button>
-        <Button className='navButton' waves='light'>Contact</Button>
-      </Navbar>
+        <div className="parent">
+          <svg id="white">
+            <text x="0" y="0">
+              <tspan x="1.8em" y="1em">CHERI'S</tspan>
+              <tspan x="1.2em" y="2.0em">CREATIVE</tspan>
+              <tspan x="0.5em" y="3.0em">CELEBRATIONS</tspan>
+            </text>
+            <pattern id="white-pattern" patternUnits="userSpaceOnUse"
+                       width="500" height="281" viewbox="0 0 500 281">
+              <image href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/131611/flower-bloom.gif" width="500" height="281" />
+            </pattern>
+          </svg>
+          <Row>
+            <Col s={12} m={12} l={12} className='navButtonsCenter'>
+              <Button className='navButton' waves='light' href="#!/" onClick={this.onContentChange}>Home</Button>
+              <Button className='navButton' waves='light' href="#!/about" onClick={this.onContentChange}>About</Button>
+              <Button className='navButton' waves='light'>Balloon Decor</Button>
+              <Button className='navButton' waves='light'>Floral Designs</Button>
+              <Button className='navButton' waves='light'>Contact</Button>
+            </Col>
+          </Row>
+      </div>
         {partial}
       <Footer className="teal lighten-1" copyrights="&copy; 2015 Copyright Cheri's Creative Celebrations. All Rights Reserved."   links={
         <ul>
