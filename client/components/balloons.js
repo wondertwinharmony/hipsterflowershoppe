5d4f8f7c539865f261a43ccfd753152c68c1253b
gschrock <gabrielschrock@gmail.com>
2016-08-24 11:59:53 -0700

    (style) Remove commented code

--- client/components/balloons.js
+++ client/components/balloons.js
@@ -2,34 +2,6 @@ import React from 'react';
 import { Component } from 'react';
 import Slider from 'react-slick';
 
-// export default class Balloons extends Component {
-//   render() {
-//       var settings = {
-//       dots: true,
-//       infinite: true,
-//       speed: 500,
-//       slidesToShow: 1,
-//       slidesToScroll: 1
-//     };
-//     return (
-//       <div id='balloons-content'>
-//         <Slider {...settings}>
-//           <div><h3>1</h3></div>
-//           <div><h3>2</h3></div>
-//           <div><h3>3</h3></div>
-//           <div><h3>4</h3></div>
-//           <div><h3>5</h3></div>
-//           <div><h3>6</h3></div>
-//         </Slider>
-//       </div>
-//     );  
-//   }
-// }
-
-
-// var React = require('react');
-// var Slider = require('react-slick');
-
 const SimpleSlider = React.createClass({
   render: function () {
     const settings = {
@@ -37,8 +9,6 @@ const SimpleSlider = React.createClass({
       infinite: true,
       speed: 500,
       centerMode: false,
-      // variableWidth: true,
-      // adapativeHeight: true,
       slidesToShow: 1,
       slidesToScroll: 1
     };
