9194b9a1abcd290cb800c8056c3dae1222ade7c6
amenotu <kyabut@gmail.com>
2016-09-07 14:08:34 -0700

    (feat) Add Mailgun API and enable contact form to send messages

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var hidden = require('./private/private.js');
+var mailgun = require('mailgun-js')({apiKey: hidden.MAILGUN_KEY , domain: hidden.MAILGUN_DOMAIN});
 
 //configure the server to use bodyparser to handle post requests
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,23 +12,30 @@ app.use(bodyParser.json());
 //set the port
 var port = process.env.PORT || 7000;
 
-app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
-
 /**
  * Back end routing set up here
  */
 
 app.post('/contact', function(req, res){
-  console.log("req.body for /contact route: ", req.body);
-  res.status(200).json({message: "Message successfully sent, thank you for your inquiry."});
+  console.log("req.body for /contact route: ", req.body, mailgun);
+  var mailOptions = {
+    from: "'" + req.body.first + " " + req.body.last + "' " + req.body.email,
+    to: "gabrielschrock@gmail.com",
+    subject: req.body.first + " " + req.body.last + " - " + req.body.subject,
+    text: req.body.message + "\n" + req.body.first + " " + req.body.last
+  };
+
+  console.log("mailOptions: ", mailOptions);
+
+  mailgun.messages().send(mailOptions, function(err, body){
+    if(err){
+      console.log(err);
+      return res.send("Error sending message");
+    } else {
+      console.log("Body from mailgun send function: ", body);
+      res.status(200).json({message: "Message successfully sent, thank you for your inquiry."});
+    }
+  });
 });
 
 //serve static assets
