89dea3e26007258078a1f1d10649c61c792d550d
Gabriel Schrock <gabrielschrock@gmail.com>
2016-11-03 11:54:22 -0700

    (fix) Optimize header and smart balloon image (#59)
    * add procfile
    
    * change server
    
    * add favicon
    
    * (fix) Optimize header and smart balloon image

--- client/components/home.js
+++ client/components/home.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
     return (
     <div id="home-content">
       <p>Cheri's Creative Celebrations specializes in balloon art and floral decorations. Our decorations will guarantee to make your function truly special. We recommend that you contact us directly to discuss your event decoration needs. We will be happy to offer you our professional advice and provide you with a quote based on your needs and budget.</p>
-      <div className="smartBalloonContainer"><a className="smartBalloonLink" href="http://www.theballooncouncil.org/smart-balloon-practices/" target="_blank"><img className="smartBalloonImg" src="https://dk-media.s3.amazonaws.com/media/1k8es/images/17044902/huge/SmartBalloonIconWeb1.jpg" alt="SmartBalloonIconWeb1.jpg"/></a></div>
+      <div className="smartBalloonContainer"><a className="smartBalloonLink" href="http://www.theballooncouncil.org/smart-balloon-practices/" target="_blank"><img className="smartBalloonImg" src="../assets/smartBalloon.jpg"/></a></div>
       <ul>
         <li className="listTitle">Balloon decorations for:</li>
         <li>Sporting events</li>
